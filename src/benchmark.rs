a4a95b4b0e8654d750b4e7d242cb8a1adde912ec
Alexius Wadell <awadell@gmail.com>
2023-06-02 21:52:50 -0400

    Add runner code
    Can now run a benchmark and extract a score from the results.
    Does store all of stdout in a string (not the best). Would be good to look at async

--- src/benchmark.rs
+++ src/benchmark.rs
@@ -1,7 +1,132 @@
-use super::environment::Environment;
+use crate::environment::Environment;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::cmp;
+use std::ffi::OsStr;
+use std::io;
+use std::io::Read;
 use std::path::PathBuf;
+use std::process::{ChildStdout, Command, Output, Stdio};
+use std::thread;
+use std::time::{Duration, Instant};
+use sysinfo::{CpuExt, CpuRefreshKind, System, SystemExt};
 
-struct Benchmark {
-    env: Environment,
-    run_script: PathBuf,
+pub struct Benchmark {
+    environment: Environment,
+    script: PathBuf,
+}
+
+impl Benchmark {
+    /// Construct a benchmark from a folder
+    pub fn from_folder(dir: &PathBuf) -> Result<Self, ()> {
+        let script = dir.join("run.sh");
+        let has_parent = match script.parent() {
+            Some(d) => d.exists(),
+            None => false,
+        };
+        if !script.is_file() && has_parent {
+            return Err(());
+        }
+        match Environment::from_folder(dir) {
+            Ok(environment) => {
+                return Ok(Benchmark {
+                    environment,
+                    script,
+                })
+            }
+            Err(_) => return Err(()),
+        };
+    }
+    /// Return the name of the benchmark
+    pub fn name(&self) -> &OsStr {
+        return self
+            .script
+            .parent()
+            .expect("run scripts should be within a folder")
+            .file_name()
+            .expect("directories have filename");
+    }
+
+    pub fn install(&self) -> Result<Output, io::Error> {
+        return self.environment.install();
+    }
+
+    fn command(&self) -> Command {
+        let mut cmd = Command::new("/bin/bash");
+        cmd.arg(self.script.file_name().unwrap())
+            .current_dir(self.script.parent().unwrap());
+        self.environment.with_env(cmd)
+    }
+
+    pub fn run(&self) -> Option<Trial> {
+        run_benchmark(self.command())
+    }
+}
+
+#[derive(Debug)]
+pub struct Trial {
+    score: f64,
+    walltime: Duration,
+    max_memory: u64,
+    cpu_usage: f32,
+}
+
+fn run_benchmark(cmd: Command) -> Option<Trial> {
+    let mut cmd = Command::from(cmd);
+    cmd.stdin(Stdio::null()).stdout(Stdio::piped());
+
+    let polling_rate = Duration::from_millis(500);
+
+    // Launch benchmark and track relevant stats
+    let mut max_memory: u64 = 0;
+    let mut sys = System::new();
+    sys.refresh_cpu_specifics(CpuRefreshKind::everything());
+    let starttime = Instant::now();
+    let mut process = match cmd.spawn() {
+        Ok(p) => p,
+        Err(e) => {
+            println!("dang {:?}", e);
+            return None;
+        }
+    };
+    while process.try_wait().unwrap().is_none() {
+        sys.refresh_memory();
+        max_memory = cmp::max(sys.total_memory(), max_memory);
+        thread::sleep(polling_rate);
+    }
+
+    // Record System Information
+    let walltime = starttime.elapsed();
+    sys.refresh_cpu_specifics(CpuRefreshKind::everything());
+    let mut cpu_usage: f32 = 0.0;
+    for cpu in sys.cpus() {
+        cpu_usage += cpu.cpu_usage();
+    }
+
+    // Look for a score
+    let score = score_benchmark(process.stdout.unwrap(), &walltime);
+
+    Some(Trial {
+        score,
+        walltime,
+        max_memory,
+        cpu_usage,
+    })
+}
+
+fn score_benchmark(mut stdout: ChildStdout, elapsed: &Duration) -> f64 {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^score: ?(?P<score>[\d,+-]+)$").unwrap();
+    }
+    let mut buffer = String::new();
+    stdout
+        .read_to_string(&mut buffer)
+        .expect("reading stdout from process");
+    let last_line = buffer.lines().last().unwrap_or("");
+    if let Some(cap) = RE.captures(last_line) {
+        return cap[1].parse::<f64>().unwrap();
+    }
+
+    // Fall Back to Daily Rate
+    return (24.0 * 60.0 * 60.0) / elapsed.as_secs_f64();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use clap::{arg, command, value_parser, ArgMatches, Command};
-use eegmark::environment::Environment;
-use std::{fs, path::PathBuf};
+use eegmark::benchmark::Benchmark;
+use std::path::PathBuf;
 
 fn main() {
     let matches = cli().get_matches();
     match matches.subcommand() {
         Some(("install", sub_matches)) => install_cmd(sub_matches),
+        Some(("run", sub_matches)) => run_cmd(sub_matches),
         _ => unreachable!(),
     }
 }
@@ -17,12 +18,16 @@ fn cli() -> Command {
         .arg_required_else_help(true)
         .subcommand(
             Command::new("install")
-                .about("Install benchmarking environments")
+                .about("Install a benchmarking environment")
                 .arg(
-                    arg!([dir] "Environment directory/directories to install")
-                        .default_value("./benchmarks")
+                    arg!([dir] "Environment directory to install")
                         .value_parser(value_parser!(PathBuf)),
                 ),
+        )
+        .subcommand(
+            Command::new("run").about("Run benchmark").arg(
+                arg!([dir] "Environment directory to run").value_parser(value_parser!(PathBuf)),
+            ),
         )
 }
 
@@ -30,17 +35,15 @@ fn install_cmd(m: &ArgMatches) {
     let dir = m
         .get_one::<PathBuf>("dir")
         .expect("Missing directory to install");
-    if !dir.is_dir() {
-        panic!("Expected {:?} to exist and be a directory", dir);
-    }
-    for entry in fs::read_dir(dir).unwrap() {
-        let path = entry.unwrap().path();
-        if let Ok(env) = Environment::from_folder(&path) {
-            print!("Found environement for {:?}", path);
-            match env.install() {
-                Ok(out) => println!("{:?}", out),
-                Err(e) => println!("{:?}", e),
-            }
-        }
-    }
+    Benchmark::from_folder(dir)
+        .expect("Not a benchmark directory")
+        .install()
+        .unwrap();
+}
+
+fn run_cmd(m: &ArgMatches) {
+    let dir = m.get_one::<PathBuf>("dir").expect("Missing directory");
+    let bench = Benchmark::from_folder(dir).expect("Not a benchmark directory");
+    let trial = bench.run().unwrap();
+    println!("Result: {} => {:?}", bench.name().to_str().unwrap(), trial);
 }
