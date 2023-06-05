98c64aef36d64e81729a9cafc22fee49e07b9c87
Alexius Wadell <awadell@gmail.com>
2023-06-05 19:26:08 -0400

    rely on eegmark to install juliaup

--- benchmarks/julia-precompile/install.sh
+++ benchmarks/julia-precompile/install.sh
@@ -4,14 +4,6 @@
 set -o pipefail
 set -e
 
-# Install Juliaup
-if ! command -v juliaup &>/dev/null; then
-	curl -fsSL https://install.julialang.org | sh -s -- --yes
-fi
-
-# Ensure the release channel is install
+# Ensure the release channel is installed
 juliaup update
 juliaup add release 2>/dev/null || true
-
-JULIA_PATH=$(dirname $(which julia))
-echo "export PATH=\$PATH:$JULIA_PATH" >activate
